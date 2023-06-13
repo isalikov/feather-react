ae60e82ab8b8f5d4a8df2fdf435343e72f5d4a1e
Iakov Salikov <salikovpro@gmail.com>
2023-06-13 12:38:04 +0100

    fix: build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,16 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 
-const packageJson = require('./package.json')
-
 export default {
     input: 'src/index.tsx',
     output: [
         {
-            file: packageJson.main,
+            file: 'dist/index.js',
             format: 'cjs',
             sourcemap: true,
         },
         {
-            file: packageJson.module,
+            file: 'dist/index.esm.js',
             format: 'esm',
             sourcemap: true,
         },
